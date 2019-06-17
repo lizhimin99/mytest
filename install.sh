09080503a0019c32ef84d928c9abc5a7e24529aa
lizhimin99 <zhimin.li@cn.dlink.com>
2019-06-17 15:20:06 +0800

    test

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TMP_NUCLIAS_CONNECT=/tmp/nuclias_connect
-
+TMP_NUCLIAS_CONNECT_config=/tmp/nuclias_connect/config
 if [ ! -d $TMP_NUCLIAS_CONNECT ];then
   mkdir -p $TMP_NUCLIAS_CONNECT
 fi
@@ -11,7 +11,10 @@ curl -o init.sh https://raw.githubusercontent.com/lizhimin99/mytest/master/init.
 curl -o docker-compose.yml https://raw.githubusercontent.com/lizhimin99/mytest/master/docker-compose.yml
 curl -o entrypoint-initdb.sh https://raw.githubusercontent.com/lizhimin99/mytest/master/entrypoint-initdb.sh
 curl -o appconfig.json https://raw.githubusercontent.com/lizhimin99/mytest/master/appconfig.json
-cd config
+if [ ! -d $TMP_NUCLIAS_CONNECT_config ];then
+  mkdir -p $TMP_NUCLIAS_CONNECT_config
+fi
+cd $TMP_NUCLIAS_CONNECT_config
 curl -o systemconfig.json https://raw.githubusercontent.com/lizhimin99/mytest/master/config/systemconfig.json
-cd ../
-./init.sh
+cd $TMP_NUCLIAS_CONNECT
+ $TMP_NUCLIAS_CONNECT"/init.sh"
