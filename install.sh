f0929e18c22ece8807fac1080e8ad8a33ebf160a
lizhimin <zhimin.li@cn.dlink.com>
2019-06-20 14:18:10 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ if [ ! -d $TMP_NUCLIAS_CONNECT ];then
 fi
 
 cd $TMP_NUCLIAS_CONNECT
-echo 'STEP 1: Start download config files'
+echo -e "\033STEP 1: Start download config files\033"
 curl -o init.sh https://raw.githubusercontent.com/lizhimin99/mytest/master/init.sh
 curl -o docker-compose.yml https://raw.githubusercontent.com/lizhimin99/mytest/master/docker-compose.yml
 curl -o entrypoint-initdb.sh https://raw.githubusercontent.com/lizhimin99/mytest/master/entrypoint-initdb.sh
@@ -18,8 +18,6 @@ fi
 cd $TMP_NUCLIAS_CONNECT_config
 curl -o systemconfig.json https://raw.githubusercontent.com/lizhimin99/mytest/master/config/systemconfig.json
 cd $TMP_NUCLIAS_CONNECT
-echo 'Config files Download complete'
-echo 'STEP2 :Please change you workdirectory and run init.sh  :
- # cd $TMP_NUCLIAS_CONNECT 
- #. init.sh '
-sudo source $TMP_NUCLIAS_CONNECT'/init.sh'
+echo -e "\033Config files Download complete\033"
+echo -e "\033STEP2 :Please change you workdirectory and run init.sh  :\033# cd $TMP_NUCLIAS_CONNECT \033#. init.sh \033"
+sudo source $TMP_NUCLIAS_CONNECT"/init.sh"
