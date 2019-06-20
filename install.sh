82659c703004724cb08213a29a0358f79d23736b
lizhimin <zhimin.li@cn.dlink.com>
2019-06-20 11:27:01 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ if [ ! -d $TMP_NUCLIAS_CONNECT ];then
 fi
 
 cd $TMP_NUCLIAS_CONNECT
+echo 'download config files'
 curl -o init.sh https://raw.githubusercontent.com/lizhimin99/mytest/master/init.sh
 curl -o docker-compose.yml https://raw.githubusercontent.com/lizhimin99/mytest/master/docker-compose.yml
 curl -o entrypoint-initdb.sh https://raw.githubusercontent.com/lizhimin99/mytest/master/entrypoint-initdb.sh
@@ -17,5 +18,6 @@ fi
 cd $TMP_NUCLIAS_CONNECT_config
 curl -o systemconfig.json https://raw.githubusercontent.com/lizhimin99/mytest/master/config/systemconfig.json
 cd $TMP_NUCLIAS_CONNECT
-
-sudo sh $TMP_NUCLIAS_CONNECT"/init.sh"
+echo 'download finished!'
+echo 'please run : ./init.sh  to install'
+#sudo sh $TMP_NUCLIAS_CONNECT"/init.sh"
